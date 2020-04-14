c417ecd9584b97f59045e57340c34a614e29d490
parkhuiwo0 <huiwo0@naver.com>
2020-04-15 00:26:29 +0900

    init : 쇼핑몰 프로젝트 초기 세팅

---
+++ src/test/java/shoppingmall/monolithicserver/MonolithicserverApplicationTests.java
@@ -0,0 +1,13 @@
+package shoppingmall.monolithicserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonolithicserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
