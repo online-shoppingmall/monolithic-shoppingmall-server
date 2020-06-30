1dc275276a87dd446c65a7df31d3957f951d3a55
sehajyang <sehajyang@gmail.com>
2020-06-30 14:27:07 +0900

    feat : product entity 작성

--- src/main/java/shoppingmall/monolithicserver/model/entity/Product.java
+++ src/main/java/shoppingmall/monolithicserver/model/entity/Product.java
@@ -3,24 +3,49 @@ package shoppingmall.monolithicserver.model.entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.LinkedHashSet;
 
 /**
  * Created by sehajyang
  * DateTime : 2020/06/29
  */
 
+@Entity
+@Table(name="product")
 @Getter
 @NoArgsConstructor
-//@Audited
 public class Product extends BaseEntity{
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="product_id")
     private Long id;
 
     private String name;
-    private String description;
 
+    @Column(length = 1000)
+    private String description;
+    private Long price;
+    private Long discountPercent;
+    private Long stockCount;
+    private Long saveStockCount;
+
+    @ManyToMany
+    private Set<Category> categories = new LinkedHashSet<>();
+
+    @OneToMany
+    private Set<ProductOptionGroups> productOptionGroups = new LinkedHashSet<>();
+
+    @OneToOne
+    @JoinColumn(name="file_id")
+    private File file;
+
+    @ManyToOne
+    @JoinColumn(name = "brand_id")
+    private Brand brand;
+    private ProductStatus status;
+
+    @Column(columnDefinition = "boolean default true")
+    private Boolean isExposure;
 }
