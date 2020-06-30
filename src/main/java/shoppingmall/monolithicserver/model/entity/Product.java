38ba8321991ef1ea716e9430c9ee0e1ee157b1d7
sehajyang <sehajyang@gmail.com>
2020-06-30 17:59:11 +0900

    feat : productOption 관련 엔티티 및 file 엔티티 작성

--- src/main/java/shoppingmall/monolithicserver/model/entity/Product.java
+++ src/main/java/shoppingmall/monolithicserver/model/entity/Product.java
@@ -2,6 +2,7 @@ package shoppingmall.monolithicserver.model.entity;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import shoppingmall.monolithicserver.model.enums.ProductStatus;
 
 import javax.persistence.*;
@@ -13,9 +14,10 @@ import java.util.Set;
  * DateTime : 2020/06/29
  */
 
+@Getter
+@Setter
 @Entity
 @Table(name="product")
-@Getter
 @NoArgsConstructor
 public class Product extends BaseEntity{
 
@@ -33,11 +35,11 @@ public class Product extends BaseEntity{
     private Long stockCount;
     private Long saveStockCount;
 
-    @ManyToMany
+    @ManyToMany(mappedBy = "")
     private Set<Category> categories = new LinkedHashSet<>();
 
     @OneToMany
-    private Set<ProductOptionsGroup> productOptionGroups = new LinkedHashSet<>();
+    private Set<ProductOptionGroup> productOptionGroups = new LinkedHashSet<>();
 
     @OneToOne
     @JoinColumn(name="file_id")
